f9379b465bad3082d857d8c7f6fbb6fe679dbf50
Georgi Koemdzhiev <koemdjiev@gmail.com>
2019-07-18 21:01:09 +0100

    Syncronising players working now

--- game_world.gd
+++ game_world.gd
@@ -8,6 +8,13 @@ func _ready():
 	
 	# Update the lblLocalPlayer label widget to display the local player name
 	$HUD/PanelPlayerList/lblLocalPlayer.text = gamestate.player_info.name
+	
+	# Spawn the players
+	if(get_tree().is_network_server()):
+		spawn_players(gamestate.player_info,1)
+	else:
+		# thell the server to spread the call to all of the connected peers so they do the spawn 
+		rpc_id(1, "spawn_players", gamestate.player_info, -1)
 
 func _on_player_list_changed():
 	# First remove all children from the boxList widget
@@ -20,3 +27,39 @@ func _on_player_list_changed():
 			var nlabel = Label.new()
 			nlabel.text = network.players[p].name
 			$HUD/PanelPlayerList/boxList.add_child(nlabel)
+
+# Spawns a new player actor, using the provided player_info structure and the given spawn index
+remote func spawn_players(pinfo, spawn_index):
+	# If the spawn_index is -1 then we define it based on the size of the player list
+	if (spawn_index == -1):
+		spawn_index = network.players.size()
+	
+	if (get_tree().is_network_server() && pinfo.net_id != 1):
+		# We are on the server and the requested spawn does not belong to the server
+		# Iterate through the connected players
+		var s_index = 1      # Will be used as spawn index
+		for id in network.players:
+			# Spawn currently iterated player within the new player's scene, skipping the new player for now
+			if (id != pinfo.net_id):
+				rpc_id(pinfo.net_id, "spawn_players", network.players[id], s_index)
+			
+			# Spawn the new player within the currently iterated player as long it's not the server
+			# Because the server's list already contains the new player, that one will also get itself!
+			if (id != 1):
+				rpc_id(id, "spawn_players", pinfo, spawn_index)
+			
+			s_index += 1
+	
+	# Load the scene and create an instance
+	var pclass = load(pinfo.actor_path)
+	var nactor = pclass.instance()
+	# Setup player customization (well, the color)
+	nactor.set_dominant_color(pinfo.char_color)
+	# And the actor position
+	nactor.position = $SpawnPoints.get_node(str(spawn_index)).position
+	# If this actor does not belong to the server, change the node name and network master accordingly
+	if (pinfo.net_id != 1):
+		nactor.set_network_master(pinfo.net_id)
+		nactor.set_name(str(pinfo.net_id))
+	# Finally add the actor into the world
+	add_child(nactor)
--- gamestate.gd
+++ gamestate.gd
@@ -3,6 +3,6 @@ extends Node
 var player_info = {
 	name = "Player",                   # How this player will be shown within the GUI
 	net_id = 1,                        # By default everyone receives "server ID"
-	actor_path = "res://player.tscn",  # The class used to represent the player in the game world
+	actor_path = "res://PlayerRoot.tscn",  # The class used to represent the player in the game world
 	char_color = Color(1, 1, 1)        # By default don't modulate the icon color
 }
